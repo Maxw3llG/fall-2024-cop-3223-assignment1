69e11a11cf12d46251cf57afc5bb08aed1f9638e
Maxw3llG <kingmaxwellg@gmail.com>
2024-09-08 18:04:46 +0000

    Completed code

--- fracturing.c
+++ fracturing.c
@@ -1,84 +1,121 @@
+//********************************************************
+// fracturing.c
+// Author: Maxwell Gayle UCFID: 5541935
+// Date: 9/7/24
+// Class: COP 3223, Professor Parra
+// Purpose: This program prints a specific message to the
+// command line. The main purpose of this assignment is to
+// check and see whether I can connect to the Codespaces Server.
+// Input: Point #1 and Point #2
+//
+// Output: (to the command line) A brief Message
+// //********************************************************
+
+
 #include<stdio.h>
 #include<math.h>
+#define PI 3.14159
 
 
-
-// functions we'll be using
- double distformula(){
-    int x1 = 0;
+// Helper function to isolate distance formula
+double distFormula(){   
+    
+    int x1 = 0;     // Declare and initialize Point #1   
     int x2 = 0;
-    int y1 = 0;
+    
+    int y1 = 0;     // Declare and initialize Point #2
     int y2 = 0;
 
-    scanf("%d\n", &x1);
-    printf("Enter X1: %d\n", x1);
-
-    scanf("%d\n", &x2);
-    printf("Enter X2: %d\n", x2);
-
-    scanf("%d\n", &y1);
-    printf("Enter y1: %d\n", y1);
-
-    scanf("%d\n", &x2);
-    printf("Enter y2: %d\n", x2); 
-        
-        
-    return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-}
-
-//     double distance = arg1;
-//     
-
-
-//     return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
-
-
-
-// function should produce x1,y1,x2,y2 and distance between points
- //double calculateDistance();{
-    
-
-    
-    
-//     double__=__;
-
-//     return__;
-//  }
- // function should produce x1,y1,x2,y2 and perimeter of enclosed space based on previous points 
-//  double calculatePerimeter();{
-//      double__=__;
-
-//      return__;
-//  }
-
- // function should produce x1,y1,x2,y2 and area of enclosed space
-// double calculateArea/();{
-//     double__=__;
+   // Prints Point #1 coordinates
    
-//     return__;
+   scanf("%d %d", &x1, &y1);
+   printf("- Point #1 entered: x1 = %d; y1 = %d\n", x1, y1);
 
-//}
-
-
-//function produces x1,y1,x2,y2 and width of enclosed space
-// double calculateWidth/();{
-//     double__=__;
-//     return__;
-// }
-// function produces x1,y1,x2,y2 and height of enclosed space
-// double calculateHeight();[
-//     double__=__;
-//     return__;
-//]
-
-// function
+   // Prints Point #2 coordinates
+   
+   scanf("%d %d", &x2, &y2);
+   printf("- Point #2 entered: x2 = %d; y2 = %d\n", x2, y2);
+    
+        
+    // Calculates distance/diameter 
+    
+    return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}   // distFormula
 
 
+// produces x1, x2, y1, y2 and the distance between 2 points
+double calculateDistance(){
+    
+    // Calculates and prints distance
+    
+    double distance = distFormula();       
+    printf("- The distance between the 2 points is %.3f\n", distance);
 
-int main(int argc, char **argv){
+    return 1.0;
+}   // calcDistance
+ 
+ 
+ // produces x1, x2, y1, y2 and the perimeter of enclosed space
+ double calculatePerimeter(){
+     
+    // Calculates and prints perimeter
+   
+    double perimeter = PI * distFormula();        
+    printf("- The perimeter of the city encompassed by your request is %.3f\n", perimeter);
 
-    double distance = distformula();
+    return 1.0; 
+    }   // calcPerimeter
+
+
+// produces x1, x2, y1, y2 and area of enclosed space
+double calculateArea(){
+
+    // Calculates radius
+     
+    double radius = 0.5 * distFormula();
+     
+     // Calculates and prints area
+     
+     double area = PI * pow(radius, 2);
+     printf("- The area of the city encompassed by your request is %.3f\n", area);
+   
+    return 2.0;
+    }   // calcArea
+
+
+// produces x1, x2, y1, y2 and width of enclosed space
+double calculateWidth(){
+    
+    // Calculates and prints width
+    
+    double width = distFormula();
+    printf("- The width of the city encompassed by your request is %.3f\n", width);
+   
+    return 1.0;
+    }   //calcWidth
+
+
+// produces x1, x2, y1, y2 and height of enclosed space
+double calculateHeight(){
+    
+    // Calculates and prints height
+
+    double height = distFormula(); 
+    printf("- The height of the city encompassed by your request is %.3f\n", height);
+    
+return 1.0;
+}   //calcHeight
+ 
+ 
+ int main(int argc, char **argv){
+
+    // Calling functions
+    double getDistance = calculateDistance();
+    double getPerimeter = calculatePerimeter();
+    double getArea = calculateArea();
+    double getWidth = calculateWidth();
+    double getHeight = calculateHeight();
 
 
     return 0;
-}
+}   // main 
